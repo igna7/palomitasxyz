25ba96b6df9083997a6b5ed76dbb346446f7f364
Ignacio Ruben <ignacioruben7@gmail.com>
2016-08-12 18:15:40 -0500

    Fix 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,10 @@ config.paperclip_defaults = {
   :url => ":s3_domain_url",
   :path => ":class/:id.:style.:extension",
   s3_credentials: {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    :s3_region => ENV('AWS_REGION'),
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    s3_region: ENV('AWS_REGION'),
   }
 }
 end
