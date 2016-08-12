18574cb7c88a424d14e369fcff3b6e93527df019
Ignacio Ruben <ignacioruben7@gmail.com>
2016-08-12 18:18:00 -0500

    Fix 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,12 @@ Rails.application.configure do
   # config/environments/production.rb
 config.paperclip_defaults = {
   storage: :s3,
-  :url => ":s3_domain_url",
-  :path => ":class/:id.:style.:extension",
+  url: ":s3_domain_url",
+  path: ":class/:id.:style.:extension",
   s3_credentials: {
-    bucket: ENV['S3_BUCKET_NAME'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    s3_region: ENV('AWS_REGION'),
-  }
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
 }
 end
